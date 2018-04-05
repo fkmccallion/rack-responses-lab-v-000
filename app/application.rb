382a03b0e4af5c751f88b8af2acba6439b89c4e0
Frank McCallion <fkmccallion@gmail.com>
2018-04-05 14:01:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-Time.now
-binding.pry
-    #if Time.now
-    #  resp.write "Good Morning!"
-    #else
-    #  resp.write "Good Afternoon!"
-    #end
+
+    if Time.now < 12:00:00
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
 
     resp.finish
   end
